c7fd0be71d4a0baf9f7998dccc628aa736556112
guilhermedionisio <gsantanadionisio@gmail.com>
2023-07-04 23:01:02 -0300

    Implement RNG and optimize Merge Sort

--- functions.cpp
+++ functions.cpp
@@ -1,10 +1,6 @@
 #include "functions.h"
 
-void merge2Vecs(std::vector<int>& numbers, const int begin, const int mid, const int end) {
-    // Auxiliary vector to sort the halves
-    const int size = end - begin + 1;
-    std::vector<int> sortedVec(size);
-
+void merge2Vecs(std::vector<int>& numbers, std::vector<int>& auxVec, const int begin, const int mid, const int end) {
     int beginStart = begin;
     int middleStart = mid + 1;
     int sortedIndex = 0;
@@ -13,37 +9,34 @@ void merge2Vecs(std::vector<int>& numbers, const int begin, const int mid, const
     // until the end of one half
     while (beginStart <= mid && middleStart <= end) {
         if (numbers[beginStart] <= numbers[middleStart]) {
-            sortedVec[sortedIndex++] = numbers[beginStart++];
+            auxVec[sortedIndex++] = numbers[beginStart++];
         } else {
-            sortedVec[sortedIndex++] = numbers[middleStart++];
+            auxVec[sortedIndex++] = numbers[middleStart++];
         }
     }
 
     // Store the remaining elements of the other half
-    while (beginStart <= mid) {
-        sortedVec[sortedIndex++] = numbers[beginStart++];
-    }
+    while (beginStart <= mid)
+        auxVec[sortedIndex++] = numbers[beginStart++];
 
-    while (middleStart <= end) {
-        sortedVec[sortedIndex++] = numbers[middleStart++];
-    }
+    while (middleStart <= end)
+        auxVec[sortedIndex++] = numbers[middleStart++];
 
     // Store the elements sorted in the original numbers vector
     // Offset the idx because they may not have the same size
-    for (auto idx = begin; idx < end + 1; idx++) {
-        numbers[idx] = sortedVec[idx - begin];
-    }
+    for (int i = 0; i < sortedIndex; i++)
+        numbers[begin + i] = auxVec[i];
 }
 
-void mergeSort(std::vector<int>& numbers, const int begin, const int end) {
+void mergeSort(std::vector<int>& numbers, std::vector<int>& auxVec, const int begin, const int end) {
     if (end > begin) {
         int mid = begin + (end - begin)/2;
         // Sort the first half of the vector recursively
-        mergeSort(numbers, begin, mid);
+        mergeSort(numbers, auxVec, begin, mid);
         // Sort the second half of the vector recursively
-        mergeSort(numbers, mid+1, end);
+        mergeSort(numbers, auxVec, mid+1, end);
         // Merge the 2 halves with the right sort
-        merge2Vecs(numbers, begin, mid, end);
+        merge2Vecs(numbers, auxVec, begin, mid, end);
     }
 }
 
@@ -103,4 +96,17 @@ void bubbleSort(std::vector<int>& numbers) {
         if (!swapped)
             break;
     }
+}
+
+std::vector<int> generateRandomNumbers(unsigned int size) {
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> distribution(-100000, 100000);
+
+    std::vector<int> numbers(size);
+    for (unsigned int i = 0; i < size; i++) {
+        numbers[i] = distribution(generator);
+    }
+
+    return numbers;
 }
--- functions.h
+++ functions.h
@@ -1,9 +1,12 @@
 #include <vector>
+#include <random>
 
-void merge2Vecs(std::vector<int>& numbers, const int begin, const int mid, const int end);
-void mergeSort(std::vector<int>& numbers, const int begin, const int end);
+void merge2Vecs(std::vector<int>& numbers, std::vector<int>& sortedVec, const int begin, const int mid, const int end);
+void mergeSort(std::vector<int>& numbers, std::vector<int>& auxVec, const int begin, const int end);
 
 int partition(std::vector<int>& numbers, const int begin, const int end);
 void quickSort(std::vector<int>& numbers, const int begin, const int end);
 
 void bubbleSort(std::vector<int>& numbers);
+
+std::vector<int> generateRandomNumbers(unsigned int size);
