485254a960206fcc80fadeb0ecd262b80f10e834
guilhermedionisio <gsantanadionisio@gmail.com>
2023-07-08 14:08:45 -0300

    Improve Code modularization

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,58 +1,32 @@
-#include <iostream>
-#include <chrono>
-#include <fstream>
 #include "functions.h"
 
 int main() {
-    std::vector<unsigned int> N = {100000, 10000000, 1000000000};
+    // Variables to test sorting
+    std::vector<float> numbers = {5.2, 2.5, 3.7, 2.8, 5.4, 7.2, 4.5, 20.0, 20.0, 19.66};
+    std::vector<float> numbersCopy1 = numbers;
+    std::vector<float> numbersCopy2 = numbers;
+    std::vector<float> Vec(numbers.size());
+
+    // Variables to measure time
+    std::vector<unsigned int> N = {E5, E7};
+    std::vector<long> measTimes = {0, 0, 0};
     std::ofstream file("sort_times.csv");
+
+    /* ----------------- SORT TEST -------------------------- */
+
+    sortingTest(numbers);
+
+    /* -------------------- MEASURE TIME ----------------------------- */
 
     // Header Row
     file << "N,Merge Sort,Quick Sort,Bubble Sort" << std::endl;
 
+    // Measure time for N < 1e9
     for (unsigned int i = 0; i < N.size(); i++) {
-        std::vector<float> numbersMerge = generateRandomNumbers(N[i]);
-        std::vector<float> numbersQuick = numbersMerge;
-        std::vector<float> numbersBubble = numbersMerge;
-
-        std::cout << std::endl;
-        std::cout << "N = " << N[i] << std::endl;
-        std::cout << std::endl;
-
-        /* --------------------- MERGE SORT --------------------------- */
-
-        auto start = std::chrono::high_resolution_clock::now();
-        std::vector<float> auxVec(numbersMerge.size());
-        mergeSort(numbersMerge, auxVec, 0, numbersMerge.size() - 1);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto durationMerge = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-        std::cout << "Merge Sort Time: " << durationMerge.count() << " ms" << std::endl;
-
-        /* --------------------- QUICK SORT --------------------------- */
-
-        start = std::chrono::high_resolution_clock::now();
-        quickSort(numbersQuick, 0, numbersQuick.size() - 1);
-        end = std::chrono::high_resolution_clock::now();
-        auto durationQuick = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-        std::cout << "Quick Sort Time: " << durationQuick.count() << " ms" << std::endl;
-
-        /* --------------------- BUBBLE SORT --------------------------- */
-
-        start = std::chrono::high_resolution_clock::now();
-        bubbleSort(numbersBubble);
-        end = std::chrono::high_resolution_clock::now();
-        auto durationBubble = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-        std::cout << "Bubble Sort Time: " << durationBubble.count() << " ms" << std::endl;
-
-        /* --------------------- ADD TO FILE --------------------------- */
-
-        // Column = Time duration of sorting
-        // Row = Qty of numbers generated
-        file << N[i] << "," << durationMerge.count() << "," << durationQuick.count() << "," << durationBubble.count() << std::endl;
+        measTimes = measureSortTime(N[i], measTimes);
     }
+    // Estimate time for N == 1e9
+    estimateSortTime(E9, measTimes);
 
     file.close();
     return 0;
